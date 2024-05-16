dd6c8db63e362a487ff63e8b3f594752a3704331
Eslam Ismail <eslam.egy.94@gmail.com>
2024-05-16 18:54:52 +0300

    milestone2 v1

--- app/course/page.tsx
+++ app/course/page.tsx
@@ -11,7 +11,7 @@ export default function Course() {
             <div>
                 <h1>Lesson Header</h1>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry
-                Lorem Ipsum has been the industrys standard dummy text ever since the 1500s
+                Lorem Ipsum has been the industrys standard dummy text ever since the 1501s
                 when an unknown printer took a galley of type and scrambled it to make a type specimen book
                 It has survived not only five centuries but also the leap into electronic typesetting
                 remaining essentially unchanged It was popularised in the 1960s with the release of Letraset sheets containing 
