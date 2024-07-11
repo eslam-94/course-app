352a7b34c33c534f8060c10a117220bda1a29335
Eslam Ismail <eslam.egy.94@gmail.com>
2024-07-12 02:11:17 +0300

    dynamic render16

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'Content-Security-Policy',
+                value: "connect-src 'self' https://firestore.googleapis.com;",
+              },
+              {
+                key: 'X-Frame-Options',
+                value: 'DENY',
+              },
+              {
+                key: 'X-XSS-Protection',
+                value: '1; mode=block',
+              },
+              {
+                key: 'X-Content-Type-Options',
+                value: 'nosniff',
+              },
+              {
+                key: 'Referrer-Policy',
+                value: 'strict-origin-when-cross-origin',
+              },
+              {
+                key: 'Permissions-Policy',
+                value: 'geolocation=()',
+              },
+            ],
+          },
+        ];
+      }
 };
 
 module.exports = nextConfig;
